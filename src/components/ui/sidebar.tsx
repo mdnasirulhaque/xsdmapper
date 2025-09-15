38b5d0a663c01ac1f59f62e542cf2770e7e836cb
Md Nasirul Haque <talk2lurisan@gmail.com>
2025-09-15 13:02:32 +0000

    On clicking the other buttons sidebar is toggling the state, this should

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -504,7 +504,7 @@ const SidebarMenuItem = React.forwardRef<
   <li
     ref={ref}
     data-sidebar="menu-item"
-    className={cn("group/menu-item relative", className)}
+    className={cn("group/menu-item relative flex justify-center", className)}
     {...props}
   />
 ))
