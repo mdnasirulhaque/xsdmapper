8be7fd280c6edb976450394dc984660d07cf2ba6
Md Nasirul Haque <talk2lurisan@gmail.com>
2025-09-17 02:16:00 +0000

    give same background color as the sidebar to the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import { CodeXml } from "lucide-react"
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between h-16 px-6 bg-card border rounded-lg shadow-sm">
+    <header className="flex items-center justify-between h-16 px-6 bg-header text-header-foreground rounded-lg shadow-sm">
         <div className="flex items-center gap-3">
             <CodeXml className="h-8 w-8 text-primary" />
             <h1 className="text-xl font-bold tracking-tight">
